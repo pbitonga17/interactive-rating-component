f70fdd741f7bc04a74fd267802e802a2a06138ee
PatriceBitonga <p.bitonga@gmail.com>
2022-06-27 19:49:09 -0600

    added js

--- javascript/index.js
+++ javascript/index.js
@@ -1,2 +1,30 @@
 'use strict';
 
+const selectBtn = document.querySelectorAll('.btn');
+const submitBtn = document.querySelector('.submit');
+const thankuBox = document.querySelector('.container2');
+const ratingBox = document.querySelector('.container1');
+const selectedRating = document.querySelector('.selected');
+
+let rating = 0;
+
+//Rating Box
+selectBtn.forEach(selectBtn => {
+    selectBtn.addEventListener('click', () => {
+        console.log(selectBtn.textContent);
+        rating = selectBtn.textContent;
+    })
+})
+
+
+//Thank you box
+submitBtn.addEventListener('click', () => {
+    ratingBox.style.display = 'none';
+    thankuBox.style.display = 'block';
+
+    if (rating !== 0) {
+        selectedRating.textContent = `You selected ${rating} out of 5`;
+    } else {
+        selectedRating.textContent = `PLEASE RATE! :)`;
+    } 
+})
